31ae2bfdd4d24016913f91278cd4361e45bac246
João Gomes <joaoeduardo.gomes21@gmail.com>
2022-01-12 19:31:10 -0300

    "programando na linguagem da web" - finished lesson 4

--- alura/02programandoNaLinguagemDaWeb/js/principal.js
+++ alura/02programandoNaLinguagemDaWeb/js/principal.js
@@ -4,32 +4,51 @@ titulo.textContent = "Nutricionismo Aparecida";
 
 /* ----------------------------- */
 //encontrando o paciente "paulo":
-var paciente = document.querySelector("#primeiro-paciente");
+//var paciente = document.querySelector("#primeiro-paciente");
+// ENCONTRANDO TODOS OS PACIENTES:
+var pacientes = document.querySelectorAll(".paciente");
 
 // encontrando peso e altura de "Paulo". Aqui pulei uma etapa definindo as variáveis enquanto já pegava o texto das td.
-var peso = paciente.querySelector(".info-peso").textContent;
-var altura = paciente.querySelector(".info-altura").textContent;
+//var peso = paciente.querySelector(".info-peso").textContent;
+//var altura = paciente.querySelector(".info-altura").textContent;
 
-// Pegando o campo de IMC na tabela do HTML:
-var tdImc = paciente.querySelector(".info-imc");
+// USANDO LOOP PARA PEGAR OS DADOS DE TODOS OS PACIENTES:
+for(var i = 0; i< pacientes.length; i++) {
+    //pegar o paciente na posição "i" da lista de pacientes:
+    var paciente = pacientes[i];
 
-//validando os dados:
-var pesoValido = true;
-var alturaValida = true;
+    // encontrando peso e altura dos pacientes:
+    var peso = paciente.querySelector(".info-peso").textContent;
+    var altura = paciente.querySelector(".info-altura").textContent;
 
-if (peso <= 0 || peso >= 1000) {
-    console.log("Peso inválido");
-    tdImc.textContent = "Peso inválido!";
-    pesoValido = false;
-}
+    
+    // Pegando o campo de IMC na tabela do HTML:
+    var tdImc = paciente.querySelector(".info-imc");
 
-if (altura <= 0 || altura >= 3) {
-    console.log("Altura inválida");
-    tdImc.textContent = "Altura inválida!";
-    alturaValida = false;
-}
+    //validando os dados:
+    var pesoValido = true;
+    var alturaValida = true;
 
-if(pesoValido && alturaValida) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido");
+        tdImc.textContent = "Peso inválido!";
+        pesoValido = false;
+
+        //Atribui a classe para aplicar a modificação de estilo do CSS quando for o caso:
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (altura <= 0 || altura >= 3) {
+        console.log("Altura inválida");
+        tdImc.textContent = "Altura inválida!";
+        alturaValida = false;
+
+         //Atribui a classe para aplicar a modificação de estilo do CSS quando for o caso:
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(pesoValido && alturaValida) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2); //O "toFixed" vai reduzir as casas decimais para 2.
+    }
 }
