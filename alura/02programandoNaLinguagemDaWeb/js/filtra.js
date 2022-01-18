c21f90a330986a21f989eae0b2b6f7791f10016c
João Gomes <joaoeduardo.gomes21@gmail.com>
2022-01-17 23:51:55 -0300

    "programando na linguagem da web" - lesson 9 almost done

--- alura/02programandoNaLinguagemDaWeb/js/filtra.js
+++ alura/02programandoNaLinguagemDaWeb/js/filtra.js
@@ -8,14 +8,23 @@ campoFiltro.addEventListener("input", function() {
             var paciente = pacientes[i];
             var tdNome = paciente.querySelector(".info-nome");
             var nome = tdNome.textContent;
+            
+            // opção A:
             var expressao = new RegExp(this.value, "i");
 
-            // Adição aqui
             if (expressao.test(nome)) {
                 paciente.classList.remove("invisivel");
             } else {
                 paciente.classList.add("invisivel");
             }
+
+            //opção B:
+            /*var comparavel = nome.substr(0, this.value.length);
+            if (!(this.value == comparavel)) {
+                paciente.classList.add("invisivel");
+            } else{
+                paciente.classList.remove("invisivel");
+            }*/
         }
     } else {
         for (var i = 0; i < pacientes.length; i++) {
