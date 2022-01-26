cebc1b995c7378631395b0d1bca88419a41080f3
João Gomes <joaoeduardo.gomes21@gmail.com>
2022-01-26 11:15:43 -0300

    starting JS-objects

---
+++ alura/00bObjetosJS/01Objetos.js
@@ -0,0 +1,12 @@
+const listaCPFs = ['189654185', '654789123', '147963582'];
+
+//const cliente = ['Nome', 'Caio', 'Idade', 36]
+
+const cliente = {
+    nome: 'Caio',
+    idade: 36,
+    cpf: 546987213,
+    email: 'algumemail@mail.com'
+};
+
+console.log(cliente);
---
+++ alura/00bObjetosJS/02DadosCliente.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome: 'Caio',
+    idade: 34,
+    cpf: '546987213',
+    email: 'algumemail@mail.com'
+};
+
+console.log(`Meu nome é ${cliente.nome}, e tenho ${cliente.idade} anos.`);
+console.log(`CPF: ${cliente.cpf.substring(0,3)}******`);
