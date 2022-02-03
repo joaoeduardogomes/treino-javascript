a82000ce0e4c69ca56cda966b66602839a386cfe
João Gomes <joaoeduardo.gomes21@gmail.com>
2022-02-03 19:13:23 -0300

    JS heritage - finished

--- alura/04InterfacesEHerancaEmOO/sistemaAutenticacao.js
+++ alura/04InterfacesEHerancaEmOO/sistemaAutenticacao.js
@@ -4,6 +4,15 @@ Ser autenticável significa ter o método "autenticar".
 
 export class SistemaAutenticacao {
     static login(autenticavel, senha) {
-        return autenticavel.autenticar(senha);
+        if(SistemaAutenticacao.ehAutenticavel(autenticavel)) {
+            return autenticavel.autenticar(senha);
+        }
+        else{
+            return false;
+        }
+    }
+
+    static ehAutenticavel(autenticavel) {
+        return "autenticar" in autenticavel && autenticavel.autenticar instanceof Function
     }
 }
