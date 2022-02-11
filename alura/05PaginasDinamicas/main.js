a700656aeba8ae605b5f6999ed2b42ddfc281346
João Gomes <joaoeduardo.gomes21@gmail.com>
2022-02-11 18:41:59 -0300

    JS dynamic pages - finished lesson 3

--- alura/05PaginasDinamicas/main.js
+++ alura/05PaginasDinamicas/main.js
@@ -2,5 +2,18 @@ function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla'); // seleciona todas as teclas do html.
 
+while() {
+    listaDeTeclas[0].onclick = tocaSomPom;
+}
+
+
+
+
+/* Pegando botão por botão:
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom; */
